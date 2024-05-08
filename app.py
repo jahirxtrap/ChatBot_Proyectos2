cbf3e9ceefd9c9ddefaf5a793c4fe7bc803ad510
jahirxtrap <jahirroman2002@gmail.com>
2024-05-07 20:14:53 -0500

    recomendaciones aleatorias y arreglos

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 from judini.codegpt import CodeGPTPlus
 from dotenv import load_dotenv
+import random
 import pickle
 import os
 load_dotenv()
 
 # Configuración de la página
 st.set_page_config(layout="wide", page_icon="./media/logo.png", page_title="Asistente de Titulación UNL")
+
+# Cargar o crear directorio de datos
+data_dir = "./data/"
+if not os.path.exists(data_dir):
+    os.makedirs(data_dir)
 
 # Conectar con CodeGPT
 if os.path.exists(".env"):
@@ -19,14 +25,25 @@ else:
     org_id= st.secrets['ORG_ID']
 codegpt = CodeGPTPlus(api_key=api_key, org_id=org_id)
 
+# Recomendaciones aleatorias
+def random_questions():
+    try:
+        with open(os.path.join(data_dir, "questions"), "rb") as file:
+            lines = file.readlines()
+            lines = [line.decode('utf-8').strip() for line in lines]
+            random.shuffle(lines)
+            return lines[:3]
+    except FileNotFoundError:
+        return [
+            "¿Cuál es el fundamento de los proyectos de integración de saberes?",
+            "¿Cuál es el propósito de la guía para la escritura y presentación del informe del trabajo de integración curricular o de titulación?",
+            "¿Cuál es la estructura del proyecto de investigación de integración curricular o de titulación?"
+        ]
+
 # Inicialización de sesiones/variables
 def initialize_session_state():
-    data_dir = "./data/"
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
-    file_path = os.path.join(data_dir, "chats")
     try:
-        with open(file_path, "rb") as file:
+        with open(os.path.join(data_dir, "chats"), "rb") as file:
             session_state_data = pickle.load(file)
             if "chat_history" in session_state_data:
                 st.session_state.chat_history = session_state_data["chat_history"]
@@ -34,6 +51,8 @@ def initialize_session_state():
                 st.session_state.current_chat = 1
             if "chat_number" in session_state_data:
                 st.session_state.chat_number = session_state_data["chat_number"]
+            if "questions" in session_state_data:
+                st.session_state.questions = session_state_data["questions"]
     except FileNotFoundError:
         if "chat_history" not in st.session_state:
             st.session_state.chat_history = []
@@ -41,13 +60,16 @@ def initialize_session_state():
             st.session_state.current_chat = 1
         if "chat_number" not in st.session_state:
             st.session_state.chat_number = 1
+        if "questions" not in st.session_state:
+            st.session_state.questions = random_questions()
 
 initialize_session_state()
 
 def save_session_state():
     session_state_data = {
         "chat_history": st.session_state.chat_history,
-        "chat_number": st.session_state.chat_number
+        "chat_number": st.session_state.chat_number,
+        "questions": st.session_state.questions
     }
     with open("./data/chats", "wb") as file:
         pickle.dump(session_state_data, file)
@@ -57,21 +79,29 @@ def get_bot_response(prompt):
     try:
         messages = [{"role": "user", "content": msg} for _, role, msg in st.session_state.chat_history if role == "user"]
         messages.append({"role": "user", "content": prompt})
-
         response_completion = codegpt.chat_completion(agent_id=agent_id, messages=messages, stream=False)
-
         full_response = response_completion if response_completion else "Disculpa, no pude procesar tu mensaje."
     except Exception as e:
         full_response = "Error al procesar la respuesta: {}".format(str(e))
-    
     return full_response
+
+# Función para mostrar mensajes
+def show_messages():
+    for n, sender, message in st.session_state.chat_history:
+        if n == st.session_state.current_chat:
+            if sender == "ai":
+                st.chat_message(sender, avatar="./media/bot_icon.png").write(message)
+            elif sender == "user":
+                st.chat_message(sender).write(message)
 
 # Función para enviar mensaje
 def message(chat, prompt):
     st.session_state.chat_history.append((chat, "user", prompt))
+    show_messages()
     bot_response = get_bot_response(prompt)
     st.session_state.chat_history.append((chat, "ai", bot_response))
     save_session_state()
+    st.rerun()
 
 # Barra lateral
 with st.sidebar:
@@ -82,6 +112,7 @@ with st.sidebar:
     if st.button("Nuevo chat", use_container_width=True):
         st.session_state.chat_number += 1
         st.session_state.current_chat = st.session_state.chat_number
+        st.session_state.questions = random_questions()
         save_session_state()
     st.divider()
 
@@ -106,32 +137,21 @@ st.divider()
 with st.chat_message("ai", avatar="./media/bot_icon.png"):
     st.write("Hola, soy un Bot de ayuda para consultas sobre temas de proyectos de integración curricular. ¿En qué puedo ayudarte hoy?")
 
-# Preguntas recomendadas
+# Recomendaciones
 col1, col2, col3 = st.columns(3)
-q1 = "Que es el PIS o proyectos de integración de saberes?"
-q2 = "Cual es el propósito de la guía para la escritura y presentación del informe del trabajo de integración curricular o de titulación?"
-q3 = "Cual es la estructura del proyecto de investigación de integración curricular o de titulación?"
 st.divider()
 
 # Entrada del usuario
 user_input = st.chat_input("Mensaje Chatbot...", max_chars=500)
 
 # Enviar mensajes
-if col1.button(q1, use_container_width=True):
-    message(st.session_state.current_chat, q1)
-if col2.button(q2, use_container_width=True):
-    message(st.session_state.current_chat, q2)
-if col3.button(q3, use_container_width=True):
-    message(st.session_state.current_chat, q3)
+if col1.button(st.session_state.questions[0], use_container_width=True):
+    message(st.session_state.current_chat, st.session_state.questions[0])
+if col2.button(st.session_state.questions[1], use_container_width=True):
+    message(st.session_state.current_chat, st.session_state.questions[1])
+if col3.button(st.session_state.questions[2], use_container_width=True):
+    message(st.session_state.current_chat, st.session_state.questions[2])
 if user_input:
     message(st.session_state.current_chat, user_input)
 
-# Mostrar mensajes
-for n, sender, message in st.session_state.chat_history:
-    if n == st.session_state.current_chat:
-        if sender == "ai":
-            with st.chat_message(sender, avatar="./media/bot_icon.png"):
-                st.write(message)
-        elif sender == "user":
-            with st.chat_message(sender):
-                st.write(message)
+show_messages()
